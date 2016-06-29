9400cc0db8f7ef5f45b68af9334d931149994557
Julieta Campos <jcampos@itexico.net>
2016-06-29 12:27:38 -0500

    Inputs en Two Way Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,3 +21,14 @@ export class Contact {
 	email: string;
 	// favourite: boolean;
 }
+
+const CONTACTS: Contact[] = [
+  {id: 1, name: 'Teni', email: 'vteniente@itexico.net'},
+  {id: 2, name: 'Perla', email: 'pfranco@itexico.net'},
+  {id: 3, name: 'Sevilla', email: 'fsevilla@itexico.net'},
+  {id: 4, name: 'Isra', email: 'igarcia@itexico.net'},
+  {id: 5, name: 'Uriel', email: 'ucoria@itexico.net'},
+  {id: 6, name: 'Braulio', email: 'blomeli@itexico.net'},
+  {id: 7, name: 'Jonathan', email: 'jcasarrubias@itexico.net'},
+  {id: 8, name: 'Javi', email: 'jsalazar@itexico.net'},
+];
